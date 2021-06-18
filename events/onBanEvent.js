3a23b6b083451a10ffc905a754ee67a02f0b898e
Abyeon <wpseals@gmail.com>
2021-06-17 21:01:10 -0400

    Added saving banned viewers to a database.

--- events/onBanEvent.js
+++ events/onBanEvent.js
@@ -1,39 +1,24 @@
 const logger = require('../utils/logger.js');
 const fs = require('fs');
 
-function findObj(obj, array) {
-    array.forEach(obj => {
-        if (obj.name == obj) {
-            return obj;
-        }
-    });
-
-    return null;
-}
-
 module.exports = async(channel, username, reason, tags) => {
     logger(`${username} has been banned from ${channel.substring(1, channel.length)}'s channel.`, "INFO");
     
-    let tempData;
+    const db = require('../database.json');
+    let index = -1;
+    
+    for (var i = 0; i < db.channels.length; i++) {
+        if (db.channels[i].name == channel.slice(1)) index = i;
+    }
 
-    fs.readFile('database.json', (err, data) => {
-        if (err) throw err;
-        let database = JSON.parse(data);
+    if (index == -1) {
+        db.channels.push({"name":channel.slice(1), "bannedViewers": []});
+        index = db.channels.length - 1;
+    }
 
-        if (findObj(channel, database.channels) == null) {
-            database.channels.push(channel);
-        }
+    db.channels[index].bannedViewers.push(username);
 
-        let banlist = findObj(channel, database.channels);
-        console.log(banlist);
-        tempData = database.channels[database.channels.indexOf(banlist)].bannedUsers.push(username);
-
-        // fs.writeFileSync('database.json', JSON.stringify(database, null, 2), (err) => {
-        //     if (err) throw err;
-        // })
-    });
-
-    fs.writeFile('database.json', JSON.stringify(tempData, null, 2), (err) => {
+    fs.writeFile('database.json', JSON.stringify(db), (err) => {
         if (err) throw err;
     });
 }
